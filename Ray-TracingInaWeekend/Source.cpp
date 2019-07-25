30eaa0a7c9c7bdd7f0827957d94c244db56a25c3
alex-rowden <ARR87@pitt.edu>
2019-07-24 21:19:33 -0400

    HelloWorld Graphics edition

--- Ray-TracingInaWeekend/Source.cpp
+++ Ray-TracingInaWeekend/Source.cpp
@@ -7,7 +7,28 @@ int main() {
 
 	fb.open("ray_traced.ppm", std::ios::out);
 	std::ostream file(&fb);
-	file << "HelloWorld" << std::endl;
+	
+
+	int nx = 200;
+	int ny = 100;
+
+	file << "P3" << std::endl;
+	file << nx << " " << ny << std::endl;
+	file << 255 << std::endl;
+
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			float r = (float)i / (float)nx;
+			float g = (float)j / (float)ny;
+			float b = .2;
+
+			int ir = (int)(r * 255);
+			int ig = (int)(g * 255);
+			int ib = (int)(b * 255);
+
+			file << ir << " " << ig << " " << ib << std::endl;
+		}
+	}
 
 	fb.close();
 
